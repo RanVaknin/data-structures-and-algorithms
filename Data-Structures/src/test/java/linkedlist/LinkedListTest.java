1ca1432dba661b41a8b1f1e6d096927c9ce7dab4
RAN VAKNIN <ranvaknin0@gmail.com>
2020-01-14 13:33:00 -0800

    readme's to come

--- Data-Structures/src/test/java/linkedlist/LinkedListTest.java
+++ Data-Structures/src/test/java/linkedlist/LinkedListTest.java
@@ -1,45 +1,58 @@
 package linkedlist;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class LinkedListTest {
+    LinkedList test;
+
+    @Before
+    public void setUp() throws Exception {
+        test = new LinkedList();
+        test.insertHead(15);
+        test.insertHead(9);
+        test.insertHead(66);
+        test.insertHead(31);
+    }
 
     @Test
     public void insertHeadTest() {
-        LinkedList test = new LinkedList();
-        test.insertHead(3);
-        test.insertHead(11);
-        test.insertHead(9);
         LinkedList input = test;
-        int expected = 9;
+        int expected = 31;
         int actual = input.head.value;
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void includesTest() {
-        LinkedList test = new LinkedList();
-        test.insertHead(15);
-        test.insertHead(9);
-        test.insertHead(66);
-        test.insertHead(31);
-        LinkedList input = test;
         boolean expected = true;
-        boolean actual = input.includes(66);
-        Assert.assertEquals(expected,actual);
+        boolean actual = test.includes(66);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testToStringTest() {
-        LinkedList test = new LinkedList();
-        test.insertHead(31);
-        test.insertHead(66);
-        test.insertHead(9);
-        test.insertHead(15);
-        LinkedList input = test;
-        String expected = "{15} -> {9} -> {66} -> {31} -> NULL";
-        String actual = input.toString();
-        Assert.assertEquals(expected,actual);
+        String expected = "{31} -> {66} -> {9} -> {15} -> NULL";
+        String actual = test.toString();
+        Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void append_test() {
+        test.append(3);
+        String expected = "{31} -> {66} -> {9} -> {15} -> {3} -> NULL";
+        String actual = test.toString();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void insertAfter_test() {
+        test.insertAfter(999,66);
+        String expected = "{31} -> {66} -> {999} -> {9} -> {15} -> NULL";
+        String actual = test.toString();
+        Assert.assertEquals(expected, actual);
+    }
+
+    
 }
