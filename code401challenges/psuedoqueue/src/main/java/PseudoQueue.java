e34d078f5318693edf02d7069fb99da8ea71e73e
RAN VAKNIN <ranvaknin0@gmail.com>
2020-01-20 20:05:01 -0800

    done

---
+++ code401challenges/psuedoqueue/src/main/java/PseudoQueue.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class PseudoQueue<T> {
+    public Stack<T> stack;
+
+    public void enqueue(T input) {
+        if (stack.empty()) {
+            stack.push(input);
+        } else {
+            stack.push(input);
+            Stack<T> stack2 = new Stack<>();
+            while (!stack.isEmpty()) {
+                T temp = stack.pop();
+                stack2.push(temp);
+            }
+            while (!stack2.isEmpty()) {
+                T temp = stack2.pop();
+                stack.push(temp);
+            }
+        }
+    }
+
+    public T deque() {
+        if (stack.isEmpty()) {
+            throw new NullPointerException("EMPTY QUEUE");
+        } else {
+            Stack<T> stack2 = new Stack<>();
+            while (!stack.isEmpty()) {
+                T temp = stack.pop();
+                stack2.push(temp);
+            }
+            T returnValue = stack2.pop();
+            while (!stack2.isEmpty()) {
+                T temp = stack2.pop();
+                stack.push(temp);
+            }
+            return returnValue;
+        }
+    }
+}
