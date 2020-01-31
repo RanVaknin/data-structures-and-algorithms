4c59d07be4b27e090a821c542967e37542eb8d42
RAN VAKNIN <ranvaknin0@gmail.com>
2020-01-30 20:49:13 -0800

    fixed findMax

--- code401challenges/src/main/java/tree/Node.java
+++ code401challenges/src/main/java/tree/Node.java
@@ -2,10 +2,15 @@ package tree;
 
 public class Node {
     Integer value;
+    String data;
     Node left;
     Node right;
 
     public Node(Integer value) {
         this.value = value;
+    }
+
+    public Node(String data) {
+        this.data = data;
     }
 }
